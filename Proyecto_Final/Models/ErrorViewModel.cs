9f8524e31e436446bdc7afe936e4a13a337f0094
malg10 <malg10@PC85>
2024-09-10 20:45:58 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto_Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
