7260e1f9346f2ba5d44ee7f0c96afe26fc107909
Lautaro Gaston Vijande <lautarovijande@itisjt.edu.ar>
2024-09-30 21:11:07 -0300

    nuevas pruebas

--- Proyecto_Final/Controllers/HomeController.cs
+++ Proyecto_Final/Controllers/HomeController.cs
@@ -29,4 +29,7 @@ namespace Proyecto_Final.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    //prueba cambios efectuados
+
 }
